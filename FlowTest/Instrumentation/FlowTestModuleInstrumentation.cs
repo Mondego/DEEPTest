9f955a9868ac2624112f9ff3cc487c9203461907
Eugenia Gabrielova <genia.likes.science@gmail.com>
2017-11-15 10:19:19 -0800

    weave

--- FlowTest/Instrumentation/FlowTestModuleInstrumentation.cs
+++ FlowTest/Instrumentation/FlowTestModuleInstrumentation.cs
@@ -56,6 +56,9 @@ namespace FlowTest
                         mapOfReadPathsToWritePaths.Add(point.moduleReadPath, point.moduleReadPath);
                     }
                 }
+
+                // Weave module
+
 			}
 
 			catch (Exception e) {
---
+++ FlowTestInstrumentation/WeaveAsserts.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace FlowTestInstrumentation
+{
+    public class WeaveAsserts {
+        
+        public static void WeaveAssert ()
+        {
+            Debug.Assert(
+                condition: ??,
+                message: ??
+            );
+        }
+    }
+}
+
